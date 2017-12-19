a3e0371ebeb0258eeb25aecb6cee0f10b37d2287
Thomas Swearingen <swearin3@msu.edu>
2017-12-18 21:58:02 -0500

    change seconds to days for two metrics

--- compare-atm-openml/compare_results.py
+++ compare-atm-openml/compare_results.py
@@ -27,7 +27,7 @@ openml_beats_cv = openml_beats_cv.loc[pd.isnull(openml_beats_cv['atm_performance
 openml_beats_cv.reset_index(drop=True, inplace=True)
 
 print 'Best F1 in the first 1000 OpenML runs is better than the best ATM cv (occurs {} of {} times):'.format(len(openml_beats_cv.index), len(data.index))
-print '\tAverage time to achieve best performance in first 1000 OpenML: {} s'.format(openml_beats_cv['openml_time_to_best_f1'].mean())
+print '\tAverage time to achieve best performance in first 1000 OpenML: {} days'.format(openml_beats_cv['openml_time_to_best_f1'].mean()/float(86400))
 print '\tAverage F1 increase over ATM: {}'.format(openml_beats_cv['openml_performance_increase_cv'].mean())
 
 atm_cv_beats_openml = data.loc[pd.notnull(data['atm_performance_increase_cv'])]
@@ -50,7 +50,7 @@ openml_beats_test = openml_beats_test.loc[pd.isnull(data['atm_performance_increa
 openml_beats_test.reset_index(drop=True, inplace=True)
 
 print '\nBest F1 in the first 1000 OpenML runs is better than the best ATM test (occurs {} of {} times):'.format(len(openml_beats_test.index), len(data.index))
-print '\tAverage time to achieve best performance in first 1000 OpenML: {} s'.format(openml_beats_test['openml_time_to_best_f1'].mean())
+print '\tAverage time to achieve best performance in first 1000 OpenML: {} days'.format(openml_beats_test['openml_time_to_best_f1'].mean()/float(86400))
 print '\tAverage F1 increase over ATM: {}'.format(openml_beats_test['openml_performance_increase_test'].mean())
 
 atm_test_beats_openml = data.loc[pd.notnull(data['atm_performance_increase_test'])]
