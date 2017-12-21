71274348fe3a71f387cd74a9b75a8d6cf92977eb
Thomas Swearingen <swearin3@msu.edu>
2017-12-21 14:08:26 -0500

    select grid or gp on command line

--- modelhub_api/modelhub-api-test.py
+++ modelhub_api/modelhub-api-test.py
@@ -1,20 +1,20 @@
+import argparse
 import queries as qr
 
-# print 'These are the functions available in ATM:'
+parser = argparse.ArgumentParser(description='Explore ATM results.')
+parser.add_argument('-r', '--runtype', type=str, choices=['gp', 'grid'], default='gp',
+                     help='Which ATM run to use (gp or grid).')
+args = parser.parse_args()
 
-# funcs = mh.get_functions()
-# print '\t|{}|'.format('-'*47)
-# print '\t| {0: >15} | {1: <27} |'.format('function_id', 'name')
-# print '\t|{}|'.format('-'*47)
-# for func in funcs:
-#     print '\t| {0: >15} | {1: <27} |'.format(func[0], func[1])
-# print '\t|{}|'.format('-' * 47)
-
+if args.runtype == 'gp':
+    csv_dir = 'gp_csvs'
+else:
+    csv_dir = 'grid_csvs'
 
 
 print '\nThese are 5 datasets in the ModelHub for which SVM & KNN have been applied:'
 
-datasets = qr.get_datasets_info(csv_dir='gp_csvs', n=5, method_ids= ['classify_svm', 'classify_knn'])
+datasets = qr.get_datasets_info(csv_dir=csv_dir, n=5, method_ids= ['classify_svm', 'classify_knn'])
 print '\t+{}+'.format('-'*41)
 print '\t| {0: >3} | {1: <33} |'.format('ID', 'Dataset')
 print '\t+{}+'.format('-'*41)
@@ -26,7 +26,7 @@ print '\t+{}+'.format('-' * 41)
 
 print '\nHere are some SVM & KNN results for dataset 2:'
 
-classifier_ids = qr.get_classifier_ids(csv_dir='gp_csvs', n = 5, dataset_ids = 2, method_ids= ['classify_svm', 'classify_knn'])
+classifier_ids = qr.get_classifier_ids(csv_dir=csv_dir, n = 5, dataset_ids = 2, method_ids= ['classify_svm', 'classify_knn'])
 classifier_structs = qr.get_classifier_details(csv_dir='gp_csvs', classifier_ids=classifier_ids)
 
 print '\t|{}|'.format('-'*29)
